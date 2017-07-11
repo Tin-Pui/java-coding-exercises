8a9e4a733953ad37ee5b06215f151973a5ca3a58
Tin-Pui <tchan15@gmail.com>
2017-07-11 09:50:19 +0100

    All invoked methods now use the string array instead of the file path.

--- codingexercises/src/main/java/com/aquaq/tinpui/exercises/Runner.java
+++ codingexercises/src/main/java/com/aquaq/tinpui/exercises/Runner.java
@@ -13,7 +13,7 @@ public class Runner {
         try {
             String[] stringArray = TextFileToStringArray.getStringArray(fileName);
             System.out.println(TotalNumbers.totalNum(stringArray));
-            RecreateFiles.createNewFile(fileName, "num2.txt");
+            RecreateFiles.createNewFile(stringArray, "num2.txt");
             RecreateFiles.createReorderedFile(stringArray, "num3.txt");
             RecreateFiles.createRemoveEmptyLinesFile(stringArray, "num4.txt");
         } catch (IOException exception) {
